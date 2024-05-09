eb92cbe234ef3ed9282810a70d55802fd07f98c9
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-09 11:30:49 +0300

    chore: Update event item component with icons and button styling

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,9 @@
-import Link from "next/link";
-
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
+
+import DateIcon from "../icons/date-icon";
+import AddresIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem(props) {
     const { title, image, date, location, id } = props;
@@ -23,13 +25,20 @@ export default function EventItem(props) {
                 <div className={classes.summary}>
                     <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{humanReadableDate}</time>
                     </div>
                     <div className={classes.address}>
+                        <AddresIcon />
                         <address>{formattedAddress}</address>
                     </div>
                     <div className={classes.actions}>
-                        <Button link={exploreLink}>Explore Event</Button>
+                        <Button link={exploreLink}>
+                            <span>Explore Event</span>
+                            <span className={classes.icon}>
+                                <ArrowRightIcon />
+                            </span>
+                        </Button>
                     </div>
                 </div>
                 <div></div>
