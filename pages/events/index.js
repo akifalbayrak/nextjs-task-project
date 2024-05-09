ccb20a99ae3367719d71ec8b9ace440abd5161af
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-09 12:18:25 +0300

    chore: Refactor Button component to handle both link and button functionality

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,20 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
+
 export default function AllEventsPage() {
+    const event = getAllEvents();
+    const router = useRouter();
+
+    function findEventsHandler(year, month) {
+        const fullpath = `/events/${year}/${month}`;
+        router.push(fullpath);
+    }
     return (
         <div>
-            <h1>All Events</h1>
+            <EventsSearch onSearch={findEventsHandler} />
+            <EventList items={event} />
         </div>
     );
 }
