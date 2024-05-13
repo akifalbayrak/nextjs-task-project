1202921938bf0dc319a413b1919a6afc40e568a8
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-14 02:11:26 +0300

    chore: Update package.json and pages/events/index.js

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,29 +1,33 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
-import { useRouter } from "next/router";
 
 export default function AllEventsPage(props) {
     const { events } = props;
     const router = useRouter();
 
     function findEventsHandler(year, month) {
-        const fullpath = `/events/${year}/${month}`;
-        router.push(fullpath);
+        const fullPath = `/events/${year}/${month}`;
+
+        router.push(fullPath);
     }
     return (
-        <div>
+        <Fragment>
             <EventsSearch onSearch={findEventsHandler} />
             <EventList items={events} />
-        </div>
+        </Fragment>
     );
 }
 
 export async function getStaticProps() {
-    const allEvents = getAllEvents();
+    const events = await getAllEvents();
+
     return {
         props: {
-            events: allEvents,
+            events: events,
         },
         revalidate: 60,
     };
