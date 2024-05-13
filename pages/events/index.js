befed1f66a58609773f7998eb658863f42b50dec
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-14 01:39:23 +0300

    chore: Update AllEventsPage to use getAllEvents from api-util

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,10 @@
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { useRouter } from "next/router";
 
-export default function AllEventsPage() {
-    const event = getAllEvents();
+export default function AllEventsPage(props) {
+    const { events } = props;
     const router = useRouter();
 
     function findEventsHandler(year, month) {
@@ -14,7 +14,17 @@ export default function AllEventsPage() {
     return (
         <div>
             <EventsSearch onSearch={findEventsHandler} />
-            <EventList items={event} />
+            <EventList items={events} />
         </div>
     );
+}
+
+export async function getStaticProps() {
+    const allEvents = getAllEvents();
+    return {
+        props: {
+            events: allEvents,
+        },
+        revalidate: 60,
+    };
 }
