637c9a5a538159e70e341ecf77ada0940a7de76f
akif albayrak <akifalbayrak6161@gmail.com>
2024-05-17 15:42:40 +0300

    Refactor API endpoint for comments in event detail page

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,39 @@
+export default function handler(req, res) {
+    if (req.method === "POST") {
+        const { email, name, text } = req.body;
+
+        if (
+            !email.includes("@") ||
+            !name ||
+            name.trim() === "" ||
+            !text ||
+            text.trim() === ""
+        ) {
+            res.status(422).json({ message: "Invalid input" });
+            return;
+        }
+
+        const newComment = {
+            id: new Date().toISOString(),
+            email,
+            name,
+            text,
+        };
+        console.log(newComment);
+        res.status(201).json({ message: "Added comment", comment: newComment });
+    }
+    if (req.method === "GET") {
+        const dummyList = [
+            {
+                id: "c1",
+                name: "akif",
+                text: "hello text",
+            },
+            {
+                id: "c2",
+                name: "mehmet",
+                text: "hello test",
+            },
+        ];
+    }
+}
